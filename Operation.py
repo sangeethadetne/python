303357d888cb1bc3a3a9eca36cf8454f4b9be1b8
s0d0cb6 <s0d0cb6@homeoffice.wal-mart.com>
2024-11-17 11:40:17 -0600

    Update Operation.py

---
+++ Operation.py
@@ -0,0 +1,14 @@
+first_name ='Sangeetha D'
+Age = 24
+Company = 'Walmart'
+monthly_Income = '20'
+monthly_Income_New  = 30
+total_Salary=None
+
+print(first_name)
+print(Age)
+print(Company)
+# typecasting as monthly_Income is string
+print(float(monthly_Income)+ float(monthly_Income) * 0.1)
+print(float(monthly_Income_New)+ (float(monthly_Income_New) * 0.1))
+print(total_Salary)
