7bf76bb010b73235c85be365818e1928653c0d90
s0d0cb6 <s0d0cb6@homeoffice.wal-mart.com>
2024-11-17 12:06:44 -0600

    update strings.py

--- Strings.py
+++ Strings.py
@@ -2,8 +2,11 @@ firstN="Sangeetha"
 lastN="Detne"
 
 print(firstN+lastN)
+
+print ("--------" *6)
 """this is a multi comment syntax"""
 """ this is a an another way to combine  the strings in sentences """
 print("My first name is " + firstN + " last name is " + lastN)
 # string ofrmatter i sbeing used here #
+print ("--------" *6)
 print(f"My first name is  {firstN} last name is  {lastN}")
