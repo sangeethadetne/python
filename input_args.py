a1592964fd5fcfb3fc37847c6a26c9971f681ee1
s0d0cb6 <s0d0cb6@homeoffice.wal-mart.com>
2024-11-17 12:12:39 -0600

    Add input_args.py

---
+++ input_args.py
@@ -0,0 +1,6 @@
+salary = int(input(" what is the current Salary : "))
+               
+hike_Per = int(input(" what is the per of the hile offered :"))
+
+total_Annum = salary+(salary*hike_Per/100)
+print(f"The total per Annum Packale is : {total_Annum}") 
