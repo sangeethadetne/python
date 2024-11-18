55ac82e635908c1557a15af3a09c6ad7be623692
s0d0cb6 <s0d0cb6@homeoffice.wal-mart.com>
2024-11-18 15:50:26 -0600

    Add Condition.py

---
+++ Conditions.py
@@ -0,0 +1,9 @@
+marks = int(input("Enter the marks:"))
+
+if marks>35:
+    if marks > 80:
+        print(" A Grade")
+    else:
+        print("didnt get A grade")
+else:
+    print("fail")
--- StringOperations.py
+++ StringOperations.py
@@ -1,3 +1,6 @@
+import math
+
+total_Amount= -20000.06
 day_Worked = 30 
 day_Wages_Associate_Per_Hour = 30
 day_Wages_Contractor_Per_Hour = 50 
@@ -10,4 +13,15 @@ print(f"The Total Wages over all : {Total_Wages_all}")
 
 print ("-----------"*6)
 # Arthematic operation -  + ,* ,-,/,%,// - Precedence is left to right  BODMAS Rule 
-print
+avg_Wages_All=(day_Wages_Associate_Per_Hour* day_Associate_Labour + day_Wages_Contractor_Per_Hour* day_Contractor_Labour)/(day_Associate_Labour+day_Contractor_Labour)
+print(f"The avg Wages earned over all : {avg_Wages_All} ")
+
+#right side binding for power operator 
+print(2** 2** 3)
+
+day_Associate_Labour+=22
+print(day_Associate_Labour)
+
+print(math.floor(total_Amount))
+print(math.ceil(total_Amount))
+print(math.fabs(total_Amount))
