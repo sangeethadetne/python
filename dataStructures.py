a108f759c8688bc3c2b43640d78909abdd98ba00
s0d0cb6 <s0d0cb6@homeoffice.wal-mart.com>
2024-11-27 11:37:21 -0600

    Collection updated

--- dataStructures.py
+++ dataStructures.py
@@ -1,1 +1,12 @@
-#sequence types - List,Tuple,String,Range
+#Collection types as these holds more than 1 value
+#sequence types - List,Tuple,String,Range - order is important as index is being used 
+#Sequence doesn't matter Dictionary,Set - order is not  important as index is not being used 
+#List = [1,2,3,4]
+#Set = {1,2,3,4}
+#Tuple = (1,2,3,4)
+#Dictionanary = {"brand":"Iphone"}
+
+orders_df ="1,2013-07-25 00:00:00,11599,  CLOSED  "
+
+new_order_df = orders_df.split(",")[3]
+print(new_order_df.lower().strip())
